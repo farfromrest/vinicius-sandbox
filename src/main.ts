7132b1774708f648415ad70127855832b2f48332
Vinicius Rocha <viniciusrocha0996@gmail.com>
2023-09-01 15:09:48 -0300

    09-01 session

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,19 @@
 import "./style.css";
 
-console.log("test");
+function onSubmit(e) {
+  e.preventDefault();
+  const form = e.target;
+  const todo = form.elements.description.value;
+  const node = document.createElement("li");
+  const id = "" + new Date().getDate();
+  const list = document.getElementById("todos");
+  node.id = id;
+  node.innerText = todo;
+  node.addEventListener("click", function () {
+    list?.removeChild(node);
+  });
+  list?.appendChild(node);
+  form.reset();
+}
+
+document.getElementById("todo-form")?.addEventListener("submit", onSubmit);
